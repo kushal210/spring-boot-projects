3c36e96774e47e218f91cb60e40fd940a9661048
kushal210 <kushalagarwal210@gmail.com>
2024-07-22 21:57:31 +0530

    Using the @Primary annotation.

--- src/main/java/com/kushal/springboot/demo/mycoolapp/DemoController.java
+++ src/main/java/com/kushal/springboot/demo/mycoolapp/DemoController.java
@@ -13,6 +13,7 @@ public class DemoController {
     // This is CONSTRUCTOR INJECTION.
     // In case of single-constructor, @Autowired is optional.
     // When multiple bean qualifies for Injection, then we can specify the bean to be injected using the @Qualifier annotation by mentioning the bean-name.
+    // @Qualifier has higher precedence over @Primary
     @Autowired
     public DemoController(@Qualifier("tennisCoach") Coach coach){
         myCoach = coach;
--- src/main/java/com/kushal/springboot/demo/util/BaseballCoach.java
+++ src/main/java/com/kushal/springboot/demo/util/BaseballCoach.java
@@ -1,7 +1,9 @@
 package com.kushal.springboot.demo.util;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-
+// While using @Primary, the implementation classes decide among themselves, which one is the Primary type, rather than the class using the implementation.
+@Primary
 @Component
 public class BaseballCoach implements Coach{
     @Override
