fb9f3f89d7bc03521796cf88c0a0bca21450cebc
kushal210 <kushalagarwal210@gmail.com>
2024-07-22 13:09:57 +0530

    Defining and injecting custom application properties.

--- src/main/java/com/kushal/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/kushal/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -7,9 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
+    @Value("${team.name}")
+    private String teamName;
+
+    @Value("${team.description}")
+    private String teamMotto;
+
     // Expose a '/' endpoint
     @GetMapping("/")
     public String sayHello(){
         return "Hello";
+    }
+    @GetMapping("/get-team-info")
+    public String getTeamInfo(){
+        return "My Coding team is " + teamName + " and its motto is " + teamMotto;
     }
 }
