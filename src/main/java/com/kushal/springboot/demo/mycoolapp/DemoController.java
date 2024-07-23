7de9db820e01483aa99e0179371813577740a0f9
kushal210 <kushalagarwal210@gmail.com>
2024-07-23 11:44:28 +0530

    Prototype Bean declaration.

--- src/main/java/com/kushal/springboot/demo/mycoolapp/DemoController.java
+++ src/main/java/com/kushal/springboot/demo/mycoolapp/DemoController.java
@@ -9,18 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // This is CONSTRUCTOR INJECTION.
     // In case of single-constructor, @Autowired is optional.
     // When multiple bean qualifies for Injection, then we can specify the bean to be injected using the @Qualifier annotation by mentioning the bean-name.
     // @Qualifier has higher precedence over @Primary
+//    @Autowired
+//    public DemoController(@Qualifier("tennisCoach") Coach coach) {
+//        myCoach = coach;
+//    }
+
     @Autowired
-    public DemoController(@Qualifier("tennisCoach") Coach coach){
+    public DemoController(@Qualifier("cricketCoach") Coach coach,
+                          @Qualifier("cricketCoach") Coach theAnotherCoach){
         myCoach = coach;
+        anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/get-daily-workout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check-scope")
+    public String checkBeanScope(){
+        return "Comparing beans, 'myCoach<>anotherCoach' : " + myCoach.equals(anotherCoach)
+                + " | Since the Scope of CricketCoach bean is of prototype, both the references of CricketCoach Object are different.";
     }
 }
