be00d2f40ccde4384b1bfcae49e9cc1fed524f2e
kushal210 <kushalagarwal210@gmail.com>
2024-07-23 20:28:41 +0530

    PostConstruct and PreDestroy

--- src/main/java/com/kushal/springboot/demo/util/BaseballCoach.java
+++ src/main/java/com/kushal/springboot/demo/util/BaseballCoach.java
@@ -1,10 +1,13 @@
 package com.kushal.springboot.demo.util;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 // While using @Primary, the implementation classes decide among themselves, which one is the Primary type, rather than the class using the implementation.
 @Primary
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BaseballCoach implements Coach{
     @Override
     public String getDailyWorkout() {
--- src/main/java/com/kushal/springboot/demo/util/CricketCoach.java
+++ src/main/java/com/kushal/springboot/demo/util/CricketCoach.java
@@ -1,12 +1,31 @@
 package com.kushal.springboot.demo.util;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach{
+
+    // Constructor.
+    public CricketCoach(){
+        System.out.println("Inside the constructor of " + this.getClass().getSimpleName());
+    }
+    // define our init method.
+    @PostConstruct
+    public void doStartupStuff(){
+        System.out.println("Inside the PostConstruct method of: " + this.getClass().getSimpleName());
+    }
+
+    // define our destroy method.
+    // @PreDestroy doesn't work when the scope is Prototype..
+    @PreDestroy
+    public void doCleanUpStuff(){
+        System.out.println("Inside the PreDestroy method of: " + this.getClass().getSimpleName());
+
+    }
     @Override
     public String getDailyWorkout() {
         return " |<---- Practice fielding for 2 hours ---->|";
