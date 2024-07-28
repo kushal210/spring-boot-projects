2bd2baba379d6ffa22140320a6aa065a51622079
kushal210 <kushalagarwal210@gmail.com>
2024-07-28 16:02:35 +0530

    Moving Exception Handler to a separate file and using '@ControllerAdvice' annotation.

--- src/main/java/com/kushal/springboot/demo/mycoolapp/rest/StudentController.java
+++ src/main/java/com/kushal/springboot/demo/mycoolapp/rest/StudentController.java
@@ -2,11 +2,11 @@ package com.kushal.springboot.demo.mycoolapp.rest;
 
 import com.kushal.springboot.demo.mycoolapp.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +15,6 @@ import java.util.List;
 public class StudentController {
 
     public List<Student> studentList = new ArrayList<>();
-
     @PostConstruct
     public void initializeStudents(){
         studentList.add(new Student(1, "Kushal", "Kumar"));
@@ -36,27 +35,4 @@ public class StudentController {
         }
         return studentList.get(studentId);
     }
-
-    // Adding an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-       return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // Adding other exception handler.... to catch any exception  (catch all)
-    @ExceptionHandler
-    ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage("Invalid Request");
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
-
 }
